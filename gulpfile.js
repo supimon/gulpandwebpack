84e678205a52c870a7dd453d9353c410ec399d6d
Supi <supimon@gmail.com>
2019-09-01 12:16:38 +0530

    injected corresponding style and script files to each template. production config almost getting done

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require("gulp");
 const webpack = require("webpack");
 const gulpWebpack = require("webpack-stream");
+const es = require("event-stream");
 const webpackProdScriptsConfig = require("./gulpwebpack.scripts.config.prod");
 const webpackProdStylesConfig = require("./gulpwebpack.styles.config.prod");
 const del = require("del");
+const pug = require("gulp-pug");
+const inject = require("gulp-inject");
 // paths
 const paths = {
   root: "./dist",
@@ -11,10 +14,16 @@ const paths = {
     pages: "./src/pages/**/*.pug"
   },
   styles: {
-    files: "./src/pages/**/*.scss"
+    files: "./src/pages/**/*.scss",
+    page1: "./dist/page1/*.css",
+    page2: "./dist/page2/*.css",
+    commons: "./dist/commons/*.css"
   },
   scripts: {
-    files: "./src/pages/**/*.js"
+    files: "./src/pages/**/*.js",
+    page1: "./dist/page1/*.js",
+    page2: "./dist/page2/*.js",
+    commons: "./dist/commons/*.js"
   }
 };
 // clean dist
@@ -34,4 +43,37 @@ function buildStyles() {
     .pipe(gulpWebpack(webpackProdStylesConfig, webpack))
     .pipe(gulp.dest(paths.root));
 }
+// pug
+function buildPages() {
+  return gulp
+    .src(paths.templates.pages)
+    .pipe(
+      inject(
+        es.merge(gulp.src(paths.styles.page1), gulp.src(paths.scripts.page1)),
+        { name: "page1" }
+      )
+    )
+    .pipe(
+      inject(
+        es.merge(gulp.src(paths.styles.page2), gulp.src(paths.scripts.page2)),
+        { name: "page2" }
+      )
+    )
+    .pipe(
+      inject(
+        es.merge(
+          gulp.src(paths.styles.commons),
+          gulp.src(paths.scripts.commons)
+        ),
+        { name: "commons" }
+      )
+    )
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(paths.root));
+}
 exports.build = gulp.series(clean, gulp.parallel(buildScripts, buildStyles));
+exports.fullbuild = gulp.series(
+  clean,
+  gulp.parallel(buildScripts, buildStyles),
+  buildPages
+);
